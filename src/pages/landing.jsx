7478b2b99d321ce391ccc9cac6f6a33abca9886e
Nate <hunter.j.nate@gmail.com>
2021-10-04 20:37:09 -0400

    test pull

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -4,7 +4,7 @@ import Layout from '../components/shared/Layout';
 const landing = () => {
     return (
         <Layout title="Home">
-            <h2>Landing Page...</h2>
+            <h2>this pArt...</h2>
 
             <p>Welcome to this pArt (demo)</p>
 
