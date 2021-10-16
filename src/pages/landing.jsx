4d5a8aa9244b44424b81d38734dfae84a98daba2
Nate Hunter <hunter.j.nate@gmail.com>
2021-10-15 21:40:53 -0400

    landing page btn links work

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../components/shared/Layout';
 
 const landing = () => {
@@ -12,8 +13,12 @@ const landing = () => {
                     <p>Welcome to this place to share public art.</p>
 
                     <div className="landing-btns">
-                        <button id="landing-btn">Map</button>
-                        <button id="landing-btn">Gallery</button>
+                        <Link to='/map' >
+                            <button id="landing-btn">Map</button>
+                        </Link>
+                        <Link to='/gallery'>
+                            <button id="landing-btn">Gallery</button>
+                        </Link>
                     </div>
                 </div>
             </div>
