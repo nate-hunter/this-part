7ccf098708971f24b6f738079daeee2c93584be1
Nate Hunter <hunter.j.nate@gmail.com>
2021-11-05 22:34:04 -0400

    Adjusted to use user data + notes added

--- src/components/gallery/GridGallery.jsx
+++ src/components/gallery/GridGallery.jsx
@@ -11,11 +11,14 @@ const GridGallery = () => {
     if (loading) return <h3>Gallery loading...</h3>
     if (error) return <h3 style={{ color: 'red' }}>Error: {error}</h3>
 
+    console.log('data:', data)
+
 
     const displayPhotos = defaultPublicArtUrls.map(obj => {
+        // const displayPhotos = data.posts.map(obj => {
         return (
             <div className="gallery-card" style={{ backgroundImage: `url(${obj.img})` }}>
-                {obj.title}
+                <span className="card-title">{obj.title}</span>
             </div>
         )
     });
@@ -33,3 +36,12 @@ const GridGallery = () => {
 }
 
 export default GridGallery;
+
+/*
+ * What's to happen on this page:
+ * A user will see a grid of photos...
+ * When the user hovers over a photo, it'll transform slightly and the photo's title will appear...
+ * The user can click on it to enlarge it and as a modal the photo will appear as a post (similar to Instagram?)...
+ * The post will contain information about the photo as well as the option to view it on the map...
+ * The user can be directed to the photo's location on the map or exit out of the modal.
+ */
